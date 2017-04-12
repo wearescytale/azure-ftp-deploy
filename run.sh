55b1cdd67f4d604d3d0fa7d3c512d970769fc3d1
João Veiga <jveiga13@gmail.com>
2017-04-12 17:08:48 +0100

    Updated run script.

--- run.sh
+++ run.sh
@@ -7,11 +7,12 @@
 FTP_URL=$WERCKER_FTP_STAGE_DEPLOY_PUBLISH_URL
 FTP_PASSWORD=$WERCKER_FTP_STAGE_DEPLOY_PASSWORD
 FTP_USERNAME=$WERCKER_FTP_STAGE_DEPLOY_USERNAME
+OUTPUT_DIR=$WERCKER_OUTPUT_DIR
 
 echo "Test connection"
 
 echo "curl -u $FTP_USERNAME:FTP_PASSWORD $FTP_URL/"
 curl -u $FTP_USERNAME:$FTP_PASSWORD $FTP_URL/
 
-echo "find WERCKER_OUTPUT_DIR -type f -exec curl -u $FTP_USERNAME:FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;"
-find $WERCKER_OUTPUT_DIR -type f -exec curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;
+echo "find $OUTPUT_DIR -type f -exec curl -u $FTP_USERNAME:FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;"
+find $OUTPUT_DIR -type f -exec curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/ \;
